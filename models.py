42d312a8c9c1304aa3552a5a96bf1c6affabba35
Cezary <102999499+panpusto@users.noreply.github.com>
2022-09-28 22:09:42 +0200

    add class message

--- models.py
+++ models.py
@@ -77,3 +77,50 @@ class User:
         cursor.execute(sql, (self.id,))
         self._id = -1
         return True
+
+
+class Message:
+    def __init__(self, from_id, to_id, text):
+        self._id = -1
+        self.from_id = from_id
+        self.to_id = to_id
+        self.text = text
+        self._creation_date = None
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def creation_date(self):
+        return self._creation_date
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = """INSERT INTO messages(from_id, to_id, text)
+                        VALUES(%s, %s, %s) RETURNING id, creation_date"""
+            values = (self.from_id, self.to_id, self.text)
+            cursor.execute(sql, values)
+            self._id, self._creation_date = cursor.fetchone()
+            return True
+        return False
+
+    @staticmethod
+    def load_all_messages(cursor, user_id=None):
+        if user_id:
+            sql = "SELECT id, from_id, to_id, text, creation_date FROM messages WHERE id=%s"
+            cursor.execute(sql, (user_id,))
+        else:
+            sql = "SELECT id, from_id, to_id, text, creation_date FROM messages"
+            cursor.execute(sql)
+        messages = []
+        for row in cursor.fetchall():
+            id_, from_id, to_id, text, creation_date = row
+            loaded_message = Message(from_id, to_id, text)
+            loaded_message._id = id_
+            loaded_message.from_id = from_id
+            loaded_message.to_id = to_id
+            loaded_message.text = text
+            loaded_message._creation_date = creation_date
+            messages.append(loaded_message)
+            return messages
